74839a079c3cc141a40a6a758984c670958a5fb9
Hugo Castilho <hugo.p.castilho@telecom.pt>
2015-09-03 14:26:11 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="empackage",
-    version="0.4.0",
+    version="0.5.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
